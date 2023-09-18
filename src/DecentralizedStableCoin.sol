ab884cd1445cfaf1d3e29200a8c1d596380dbe4b
Kel-VinTech <mondaykelvintech@gmail.com>
2023-09-17 20:48:46 -0400

    created new contract

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifer: MIT
+// "SPDX-License-Identifer: MIT"
 
 // This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
 
@@ -25,4 +25,36 @@
 
 pragma solidity ^0.8.19;
 
-contract DecentralizedStableCoin {}
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+* @title Decentralized Stable Coin
+* @author Kelvin.eth
+* Collateral: Exogenous (ETH & BTC )
+* Minting: Algorithmic
+* Stability: Anchored (pegged to USD)
+* This is the contract to be giverned by the DSCEngine. This contract is just the ERC20 
+implementation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    // errors
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+}
