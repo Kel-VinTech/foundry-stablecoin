72c3219a1e9dfacd431a141239844d257fca4606
Kel-VinTech <mondaykelvintech@gmail.com>
2023-09-27 18:54:53 -0400

    changes

---
+++ script/DeployDsc.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+
+contract DeployDsc is Script {
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {}
+}
