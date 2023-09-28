375b0620928465d4b9aeb31454c6c5ce2e8e03fc
Kel-VinTech <mondaykelvintech@gmail.com>
2023-09-28 15:57:33 -0400

    changes

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-// import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
-// import {DSCEngine} from "../src/DSCEngine.sol";\
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -15,11 +16,15 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
-    NetworkConfig public ActiveNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+
+    NetworkConfig public activeNetworkConfig;
 
     constructor() {}
 
-    funcrion getSepoliaEthConfig() public view returns(NetworkConfig memeory){
+    function getSepoliaEthConfig() public view returns(NetworkConfig memeory){
         return NetworkConfig({
             wethUsdPriceFeed:0x694AA1769357215DE4FAC081bf1f309aDC325306,
             wbtcUsdPriceFeed:0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
@@ -27,5 +32,18 @@ contract HelperConfig is Script {
             wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
             deployerKey: vm.envUint("PRIVATE_KEY")
         })
+    }
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig){
+        if(activeNetworkConfig.wethUsdPriceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            ETH_USD_PRICE
+        );
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+        vm.stopBroadcast();
+
     }
 }
